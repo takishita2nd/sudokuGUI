2d8b5a4af084f06377f476a67c09159dbb0ce479
滝下 則幸 <n.taki.0416@gmail.com>
2020-04-07 08:49:37 +0900

    プロジェクト ファイルを追加します。

---
+++ sudokuGUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudokuGUI
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Altseedを初期化する。
+            asd.Engine.Initialize("Empty", 640, 480, new asd.EngineOption());
+
+            // Altseedが進行可能かチェックする。
+            while (asd.Engine.DoEvents())
+            {
+                // Altseedを更新する。
+                asd.Engine.Update();
+            }
+
+            // Altseedを終了する。
+            asd.Engine.Terminate();
+        }
+    }
+}
